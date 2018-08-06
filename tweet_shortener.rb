a5765c2bcaf8f6654dd3468ccaf7f47179a87acf
angelica winkler <caw400@nyu.edu>
2018-08-06 22:10:04 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+
+def dictionary
+  {"hello" => 'hi',
+  "to, two, too" => '2',
+  "for, four" => '4',
+  'be' => 'b',
+  'you' => 'u',
+  "at" => "@",
+  "and" => "&"
+}
+end
+
+
+
+def word_substituter (tweet)
+  tweet = tweet.split(" ")
+  dictionaryKeys = dictionary.keys
+
+  shorterTweet = []
+
+  tweet.each do |word|
+    i = 0
+    while i < dictionaryKeys.length
+      if word == dictionaryKeys[i]
+        shorterTweet.push(dictionary[word])
+        i += 1
+      else
+        shorterTweet.push(word)
+        i += 1
+      end
+    end
+  end
+  shorterTweet = shorterTweet.join(" ")
+
+end
