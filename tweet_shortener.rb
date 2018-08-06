dc63d6ec220f76facdbc2e67f2384d692c985567
angelica winkler <caw400@nyu.edu>
2018-08-06 22:51:33 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -49,7 +49,7 @@ def shortened_tweet_truncator (tweet)
   tweet = selective_tweet_shortener(tweet)
 
   if tweet.length > 140
-    puts "#{tweet[0..140]}..."
+    "#{tweet[0..140]}..."
   else
     puts tweet
   end
